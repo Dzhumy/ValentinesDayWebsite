38459616565800494464c442a0b9247f17933de4
Edgars Dudins <104391991+Dzhumy@users.noreply.github.com>
2024-02-08 20:56:46 +0200

    Update style.css

---
+++ style.css
@@ -0,0 +1,89 @@
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+body {
+  background-color: #fff0f6;
+}
+
+.container {
+  height: 100vh;
+  margin: 0 auto;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.main-picture {
+  width: 30rem;
+  height: 25rem;
+  margin-bottom: 2rem;
+}
+
+.subtext {
+  font-size: 2.5rem;
+  color: #f53699;
+  text-align: center;
+}
+
+.title {
+  font-size: 3.6rem;
+  color: #f53699;
+  text-align: center;
+  margin-bottom: 2rem;
+}
+
+.buttons {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+
+  gap: 1rem;
+  margin-bottom: 5rem
+}
+
+.btn {
+  padding: 1.5rem 2.5rem;
+  border: none;
+  border-radius: 4px;
+
+  color: white;
+  font-size: 1.6rem;
+  font-weight: 600;
+  cursor: pointer;
+  display: inline-block;
+}
+
+.btn--yes {
+  background-color: #40c057;
+}
+
+.btn--no {
+  background-color: #f03e3e;
+}
+
+.hidden {
+  display: none;
+}
+
+.btn--yes,
+.btn--no {
+  transition: background-color 0.3s;
+}
+
+.btn--yes:hover {
+  background-color: #12e235;
+}
+
+.btn--no:hover {
+  background-color: #ff0000;
+}
