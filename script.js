fb5b31e75b5d70627e83b8a7fc410d4350254ac9
Edgars Dudins <104391991+Dzhumy@users.noreply.github.com>
2024-02-08 20:56:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,89 +1,62 @@
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
+const titleElement = document.querySelector(".title");
+const buttonsContainer = document.querySelector(".buttons");
+const yesButton = document.querySelector(".btn--yes");
+const noButton = document.querySelector(".btn--no");
+const mainImage = document.querySelector(".main-picture");
+
+const messages = [
+  "No",
+  "Are you sure?",
+  "Pookie please",
+  "Elli please stopopopop!!!",
+  "Don't do this to me :(",
+  "You're breaking my heart",
+  "I'm gonna cry...",
+  "STOOOOP NOOWWWWW!!!"
+];
+const MAX_TEXTS = messages.length;
+
+let play = true;
+let noCount = 0;
+
+yesButton.addEventListener("click", handleYesClick);
+
+noButton.addEventListener("click", function() {
+  if (play) {
+    noCount++;
+    resizeYesButton();
+    updateNoButtonText();
+    if (noCount === MAX_TEXTS) {
+      play = false;
+    }
+  }
+});
+
+
+function handleYesClick() {
+  titleElement.innerHTML = "Yayyy!! :3";
+  buttonsContainer.classList.add("hidden");
+  changeImage("yes");
 }
 
-html {
-  font-size: 62.5%;
+function resizeYesButton() {
+  const computedStyle = window.getComputedStyle(yesButton);
+  const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
+  const newFontSize = fontSize * 1.6;
+
+  yesButton.style.fontSize = `${newFontSize}px`;
 }
 
-body {
-  background-color: #fff0f6;
+function generateMessage(noCount) {
+  const messageIndex = Math.min(noCount, messages.length - 1);
+  return messages[messageIndex];
 }
 
-.container {
-  height: 100vh;
-  margin: 0 auto;
-
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+function changeImage() {
+  mainImage.src = `https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif`;
+  mainImage.style = 'width: 30rem; height: 30rem;';
 }
 
-.main-picture {
-  width: 30rem;
-  height: 25rem;
-  margin-bottom: 2rem;
-}
-
-.subtext {
-  font-size: 2.5rem;
-  color: #f53699;
-  text-align: center;
-}
-
-.title {
-  font-size: 3.6rem;
-  color: #f53699;
-  text-align: center;
-  margin-bottom: 2rem;
-}
-
-.buttons {
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: center;
-
-  gap: 1rem;
-  margin-bottom: 5rem
-}
-
-.btn {
-  padding: 1.5rem 2.5rem;
-  border: none;
-  border-radius: 4px;
-
-  color: white;
-  font-size: 1.6rem;
-  font-weight: 600;
-  cursor: pointer;
-  display: inline-block;
-}
-
-.btn--yes {
-  background-color: #40c057;
-}
-
-.btn--no {
-  background-color: #f03e3e;
-}
-
-.hidden {
-  display: none;
-}
-
-.btn--yes,
-.btn--no {
-  transition: background-color 0.3s;
-}
-
-.btn--yes:hover {
-  background-color: #12e235;
-}
-
-.btn--no:hover {
-  background-color: #ff0000;
+function updateNoButtonText() {
+  noButton.innerHTML = generateMessage(noCount);
 }
